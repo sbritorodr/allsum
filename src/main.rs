a3055c7854f22d16887ecfa676b13fed803790f6
sbritorodr <sbritorodr@gmail.com>
2022-08-12 16:50:16 +0100

    Added sha, blake3

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,30 @@
 )]
 #![allow(non_snake_case)] // For some reason, tauri converts variables from snake_case to camelCase, creating an error of unused keys
 use md5;
+use sha2::{Sha256, Sha512, Digest};
+use blake3;
 
 
+fn allsum_sha256(sha256_input: &str) -> String{
+
+  let mut hasher = Sha256::new();
+  hasher.update(sha256_input.as_bytes());
+  let result = hasher.finalize();
+  format!("{:x}", result)
+}
+
+fn allsum_sha512(sha512_input: &str) -> String {
+  let mut hasher = Sha512::new();
+  hasher.update(sha512_input.as_bytes());
+  let result = hasher.finalize();
+  format!("{:x}", result)
+}
+
+fn allsum_blake3(blake3_input: &str) -> String {
+  eprintln!("Generating BLAKE-3 algorithm...");
+  let result = blake3::hash(blake3_input.as_bytes());
+  format!("{}", result.to_hex())
+}
 
 fn allsum_md5(md5_input: &str) -> String{
   eprintln!("Generating md5 output...");
@@ -17,6 +39,9 @@ fn allsum_md5(md5_input: &str) -> String{
 fn text_hash_processing(inputStr: &str, hashType: &str) -> String { 
   match hashType {
     "md5" => allsum_md5(inputStr),
+    "sha256" => allsum_sha256(inputStr),
+    "sha512" => allsum_sha512(inputStr),
+    "blake3" => allsum_blake3(inputStr),
     _ => format!("Hash type '{}' is not avaliable", hashType)
   }
 }

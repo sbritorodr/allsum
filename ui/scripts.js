cc8eacbf87366c225ff40547cf42fb4f7d3f69ab
sbritorodr <sbritorodr@gmail.com>
2022-08-15 12:44:24 +0100

    Added an error message

--- ui/scripts.js
+++ ui/scripts.js
@@ -1,3 +1,26 @@
+// Global functions:
+function msgBox(msg, style) {
+    const floating_box = document.getElementById('float-message');
+    const floating_box_msg = document.getElementById('float-message-text');
+
+    floating_box_msg.innerHTML = msg;
+    floating_box.hidden = false;
+    var colorBackground = ""
+    switch (style) {
+        case 'warn':
+            colorBackground = "hsl(29, 97%, 37%)" // dark orange
+            break
+        case 'ok':
+            colorBackground = "hsl(127, 79%, 30%)" // dark green
+            break
+        default:
+            colorBackground = "rgb(133, 15, 15)" // dark red
+            break
+    }
+    floating_box.style.background = colorBackground
+}
+
+
 const invoke = window.__TAURI__.invoke
 function getTextInput(){
     const input_text = document.querySelector('#text_input').value

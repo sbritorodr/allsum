900b1def5fb95745e262b8f33fea3dec6c179f00
sbritorodr <sbritorodr@gmail.com>
2022-09-25 15:59:48 +0100

    System info, input file process WIP
    Up to this point javascript will be replaced with typescript since javascript is not strong typed and It's giving me lots and lots of "null" errors for some reason.

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,12 @@
+// System Info:
+const os = window.__TAURI__.os;
+async function getSystemInfo(){
+  const platformName = await os.platform();
+  const osType = await os.type();
+
+  console.log("SYSTEM INFORMATION:\nPlatform Name: ", platformName, "\nOsType: ", String(osType).type);
+
+}
 // Global functions:
 function msgBox(msg, style, timer_ms = 5000) {
   const floating_box = document.getElementById("float-message");
@@ -8,15 +17,16 @@ function msgBox(msg, style, timer_ms = 5000) {
   floating_box_msg.innerHTML = msg;
   msgBoxStatus(false);
   var colorBackground = "";
-  switch (style) {
+  switch (style) { // warn, ok, error
     case "warn":
       colorBackground = "hsl(29, 97%, 37%)"; // dark orange
       break;
     case "ok":
       colorBackground = "#77BA99"; // dark green
       break;
-    default: // dark red
+    default: // (Error) dark red
       colorBackground = "rgb(133, 15, 15)";
+      console.error(msg);
       break;
   }
   floating_box.style.background = colorBackground;
@@ -50,15 +60,17 @@ function hashMode() {
   const containerField = document.querySelector(".input-container");
   let msgfileInputHidden = document.getElementById("input-file-msg");
   if (isFileModeOn.checked){ // File Mode:
-    inputField.type = "file"; //changes the html to display only the file option dinamically
+    inputField.type = "button"; //changes the html to display only the file option dinamically
     inputField.id = "file_input";
+    inputField.setAttribute("onclick", "inputFileProcess();") //Add the "open file" dialog function declared later
     inputField.classList.remove('text'); inputField.classList.add('file');
     containerField.classList.remove('text'); containerField.classList.add('file');
     msgfileInputHidden.style.display = 'block'
 
-  } else { // Hash Mode:
+  } else { // Text Mode:
     inputField.type = "text";
     inputField.id = "text_input";
+    inputField.removeAttribute("onlick");
     inputField.classList.remove('file'); inputField.classList.add('text');
     containerField.classList.remove('file'); containerField.classList.add('text');
     msgfileInputHidden.style.display = 'none'
@@ -78,11 +90,55 @@ function inputProcess(){
   console.log('Output bytes: \n', output);
 }
 
-/*
-function getTimeElapsed(){
-    const time_div = document.querySelector
-} */
-// File upload
+// FILE MANAGE:
+/// OPEN DIALOG AND SEND BYTES TO RUST:
+const open = window.__TAURI__.dialog.open;
+const readBinaryFile = window.__TAURI__.fs.readBinaryFile;
+async function inputFileProcess(){
+  console.log("Processing file");
+  const selected = await open({
+    multiple: false,
+    directory: false,
+    title: "Select file to be hashed",
+  });
+  if (selected == null){ // User cancelled the dialog. 
+                        //(Doesn't Work!! Gives me this error: 
+                        //"Unhandled Promise Rejection: invalid type: null, expected path string")
+    console.error("Open file dialog cancelled by user");
+  } else {
+    console.log("Selected: ", selected, "\nAnd it's a: ", selected.type); 
+  };
+  /* REGEX: https://regexr.com/*/
+  const osType = await os.type();
+  switch (osType){
+    case 'Windows_NT': // /[^\\]+(\.+)?$/gi
+      var regExString = new RegExp("[^\\]+(\.+)?$", "gi");
+    case 'Linux': // /(\w+)(\.\w+)+(?!.*(\w+)(\.\w+)+)/gi
+      var regExString = new RegExp("(\w+)(\.\w+)+(?!.*(\w+)(\.\w+)+)", "gi");
+    case 'Darwin': // /(\w+)(\.\w+)+(?!.*(\w+)(\.\w+)+)/gi (MacOS)
+      var regExString = new RegExp("(\w+)(\.\w+)+(?!.*(\w+)(\.\w+)+)", "gi");
+    default:
+      // WARNING: THIS SHOULD BE DELETED AND FIXED (TODO) WHEN I MIGRATE TO TS:
+      var regExString = new RegExp("(\w+)(\.\w+)+(?!.*(\w+)(\.\w+)+)", "gi"); //Since I'm using linux
+      msgBox("Error!. OS Not supported. " + osType)
+  };
+  // Get filename and extension: filename
+  var regExStringFilename = new RegExp(".*(?=(\.\w*$))", "gi")
+  var regExStringExtension = new RegExp("[^\.]+$", "gi");
+
+  var fileString = regExString.exec(String(selected))
+  var filename = regExStringFilename.exec(fileString); // regex: /[^\.]+$/gim
+  var extension = regExStringExtension.exec(fileString);
+  console.log("Filename: ", filename, "\nExtension: ", extension)
+
+  const fileContentInBinary = await readBinaryFile(selected); // As Uint8Array
+  if (!fileContentInBinary){
+    msgBox("Couldn't read file!");
+    return
+  }
+  msgBox("File Readed!", "ok"); // This is just a dummy. In future versions I'll work better on the frontend
+  console.log("FILE IN BINARY: ", fileContentInBinary);
+};
 
 //MISC FUNCTIONS:
 function copyToClipboard() {
@@ -92,23 +148,6 @@ function copyToClipboard() {
   msgBox("Copied the text", "ok", 2000);
 }
 
-//Open files
-const open = window.__TAURI__.dialog.open;
-const message = window.__TAURI__.dialog.message;
+// Load this functions at startup:
 
-async function inputFileProcess(){
-  console.log("Processing file");
-  
-
-  const selected = await open({
-    multiple: false,
-    
-  });
-  
-  if (selected == null){ // User cancelled the dialog
-    console.log("Open file dialog cancelled by user")
-
-  } else {
-    console.log("Selected: ", selected);
-  }
-};
+getSystemInfo()
